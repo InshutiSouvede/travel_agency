1730b20ef1864b1f86446c5a00073ec500e8e478
InshutiSouvede <isouvede2@gmail.com>
2024-07-18 18:13:00 +0200

    Add more data in a dynamic way

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,23 @@
 import './App.css'
 import Card from './components/Card'
 import Nav from './components/Nav'
+import data from './data'
 function App() {
-
+  // console.log(data)
+  const cards = data.map((el)=>{
+    return <Card 
+    img={el.img} 
+    place= {el.place} 
+    title = {el.title} 
+    duration = {el.duration} 
+    description = {el.description}
+    />
+  })
   return (
     <div className='w-[550px] m-auto'>
       <Nav />
-      <Card />
-      
-    </div>
+      {cards}
+      </div>
   )
 }
 
--- src/components/Card.jsx
+++ src/components/Card.jsx
@@ -1,20 +1,22 @@
 import fuji from '../assets/images/fuji.png'
 import map from '../assets/icons/map.svg'
-export default function Nav() {
+export default function Card(props) {
+    console.log("prope",props)
     return (
-        <section className='p-10 flex gap-[19px] '>
-            <div className="card--img w-[470px] ">
-                <img src={fuji} className='w-full' alt="fuji" />
+        <section className='p-10 grid grid-cols-[30%_60%] gap-5  '>
+            <div className="card--img">
+                {/* relative path from the public folder */}
+                <img src={`../src/assets/images/${props.img}`}  className='w-full' alt="fuji" />
             </div>
             <div className='text-xs flex flex-col justify-evenly py-5'>
                 <div className="card-- flex gap-5 ">
                     <div className='flex gap-2'><img src={map} alt="map" />
-                        <span className='uppercase'>Japan</span></div>
+                        <span className='uppercase'>{props.place}</span></div>
                     <a className='underline text-[#918E9B]' href="#">View on Google Maps</a>
                 </div>
-                <h1 className="card--title h-10 font-bold text-2xl">Mount Fuji</h1>
-                <h3 className="card--subtitle font-bold">12 Jan, 2021 - 24 Jan, 2021</h3>
-                <p className="card--description">Mount Fuji is the tallest mountain in Japan, standing at 3,776 meters (12,380 feet). Mount Fuji is the single most popular tourist site in Japan, for both Japanese and foreign tourists.</p>
+                <h1 className="card--title h-10 font-bold text-2xl">{props.title}</h1>
+                <h3 className="card--subtitle font-bold">{props.duration}</h3>
+                <p className="card--description">{props.description}</p>
             </div>
 
         </section>
